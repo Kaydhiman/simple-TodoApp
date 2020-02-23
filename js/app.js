42119932de99ecde29c7b4b586087c53a420b607
Kay dhiman <kaydhiman0@gmail.com>
2020-02-23 12:00:02 +0530

    add edit function

--- js/app.js
+++ js/app.js
@@ -1,68 +1,137 @@
-let todoForm, taskName, showTask, tasks;
+'use strict';
+let todoForm, taskName, taskDesc, showTask, tasks;
 
 todoForm = document.forms.namedItem('todo-form');
 taskName = document.querySelector('.task-name');
+taskDesc = document.querySelector('.task-desc');
 showTask = document.querySelector('.show-tasks');
 tasks = [];
 
 // constructor to make task
-let Task = function(task) {
+let Task = function(task, desc) {
     this.task = task;
-    this.id = tasks.length + 1
+    this.desc = desc;
+    this.time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
+    this.id = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 0;
 }
 
-// fetch local storage data and set into tasks array
-let fetchData = () => localStorage.getItem('task') ? tasks = JSON.parse(localStorage.getItem('task')) : tasks = [];
+// data manager to set or get data from localstorage
+let dataManager = {
 
+    fetchData : () => localStorage.getItem('task') ? tasks = JSON.parse(localStorage.getItem('task')) : tasks = [],
 
-// render funntion to render tasks
+    setData : () => {
+        if(taskName.value.length > 0 && taskName.value != ' ') {
+            let task = new Task(taskName.value, taskDesc.value);
+
+            tasks.push(task);
+
+            localStorage.setItem('task', JSON.stringify(tasks));
+        }
+    }
+}
+
+// render tasks on DOM
 let render = () => {
-    
+
     if(tasks) {
 
         showTask.innerHTML = '';
 
         for(let item in tasks) {
 
-            let taskHTML = `<h3 data-id="${tasks[item].id}">${tasks[item].task}</h3>`;
+            let taskHTML = `<div class="task-wrapper">
+                                <h3 class="task">${tasks[item].task}</h3>
+                                <p class="task-description">${tasks[item].desc}</p>
+                                <button class="edit-task">Edit</button>
+                                <button class="delete-task" data-id="${tasks[item].id}">Delete</button>
+                            </div>`;
 
             showTask.insertAdjacentHTML('beforeend', taskHTML);
         }
-        
     }
-}
-
-
-// set task to localstorage
-let setData = () => {
-    let task = new Task(taskName.value);
-    tasks.push(task);
-    localStorage.setItem('task', JSON.stringify(tasks));
 }
 
 
 // delete item from task list
 let deleteData = () => {
+
     showTask.addEventListener('click', e => {
+
         if(e.target.dataset.id){
+
             for(let i in tasks) {
+
                 if(tasks[i].id === parseInt(e.target.dataset.id)) {
+
                     tasks.splice(i,1);
+
                     localStorage.setItem('task', JSON.stringify(tasks));
+
                     render();
                 }
             }
         }
+
+        editTask(e);
     })
+}
+
+// 
+let editTask = ev => {
+    if(ev.target.tagName === 'BUTTON') {
+        let listOfEls = ev.target.parentNode.children;
+
+        for(let i = 0; i < listOfEls.length; i++) {
+            if(listOfEls[i].classList.value === 'task' || listOfEls[i].classList.value === 'task-description') {
+                listOfEls[i].contentEditable = 'true';
+                listOfEls[i].focus();
+            }
+        }
+
+        document.addEventListener('keydown', e => {
+            
+            if(e.keyCode == 13) {
+                if(e.ctrlKey) {
+                    let taskContent, taskDiscContent;
+
+                    for(let i = 0; i < listOfEls.length; i++) {
+
+                        listOfEls[i].classList.value === 'task' ? taskContent = listOfEls[i].innerText : null;
+                        
+                        listOfEls[i].classList.value === 'task-description' ? taskDiscContent = listOfEls[i].innerText : null;
+                        
+                        if(listOfEls[i].dataset.id) {
+                            for(let el = 0; el < tasks.length; el++) {
+                                
+                                if(tasks[el].id == listOfEls[i].dataset.id) {
+                                    tasks[el].task = taskContent;
+                                    tasks[el].desc = taskDiscContent;
+                                    localStorage.setItem('task', JSON.stringify(tasks));
+                                }
+                            }
+                            
+                            for(let i = 0; i < listOfEls.length; i++) {
+                                listOfEls[i].contentEditable = 'false';
+                            }
+                        }
+                    }
+
+                }
+            }
+        })
+    }
 }
 
 
 // on form submit create a new task and set into localstorage
 if (todoForm) {
+
     todoForm.addEventListener('submit', e => {
+
         e.preventDefault();
 
-        setData();
+        dataManager.setData();
 
         init();
     })
@@ -70,15 +139,19 @@ if (todoForm) {
 
 // create initial state function
 let init = () => {
+
     taskName.value = '';
+
+    taskDesc.value = '';
+
     taskName.focus();
-    fetchData();
+
+    dataManager.fetchData();
+
     render();
+
     deleteData();
 }
 
-// initial state
+// set initial state
 init();
-
-
-
